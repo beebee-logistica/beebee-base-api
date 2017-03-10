5ab3597c79a84473fecbd88d068b4341a80c7835
Juliano Petronetto <petronetto@xdevel.com.br>
2017-03-10 15:42:34 -0300

    Fix

--- config/start.sh
+++ config/start.sh
@@ -5,7 +5,7 @@ procs=$(cat /proc/cpuinfo | grep processor | wc -l)
 sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
 
 # Again set the right permissions (needed when mounting from a volume)
-chown -Rf www-data:www-data /app
+# chown -Rf www-data:www-data /app
 
 # Start supervisord and services
 exec supervisord -c /etc/supervisord.conf
